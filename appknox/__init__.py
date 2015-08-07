6091ee3694df44a32188b734432b9114feb27b8e
dhilipsiva <dhilipsiva@gmail.com>
2015-08-07 20:37:47 +0530

    Added Errors messages and sample API calls

--- appknox/__init__.py
+++ appknox/__init__.py
@@ -18,3 +18,49 @@ __status__ = "development"
 """
 Python wrapper for Appknox's REST API
 """
+
+import requests
+from appknox.errors import MissingCredentialsError, InvalidCredentialsError
+
+
+class AppknoxClient(object):
+    """
+    Appknox Client
+    """
+
+    def login(self):
+        """
+        Login and Get token
+        """
+        login_url = "%s/token/new.json" % self.api_base
+        data = {
+            'username': self.username,
+            'password': self.password,
+        }
+        response = requests.post(login_url, data=data)
+        json = response.json()
+        if not json['success']:
+            raise InvalidCredentialsError
+        self.token = json['token']
+        self.user_id = json['user']
+
+    def __init__(
+            self, username=None, password=None, api_key=None,
+            instance_domain="beta.appknox.com", secure=True, auto_login=True):
+        super(AppknoxClient, self).__init__()
+        if username and password:
+            self.basic_auth = True
+            self.username = username
+            self.password = password
+        # API auth comes later
+        # elif api_key:
+        #     self.basic_auth = False
+        #     self.api_key = api_key
+        else:
+            raise MissingCredentialsError
+        protocol = 'http'
+        if secure:
+            protocol += 's'
+        self.api_base = "%s://%s/api" % (protocol, instance_domain)
+        if auto_login:
+            self.login()
---
+++ appknox/errors.py
@@ -0,0 +1,45 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# vim: fenc=utf-8
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
+#
+#
+
+"""
+File name: errors.py
+Version: 0.1
+Author: dhilipsiva <dhilipsiva@gmail.com>
+Date created: 2015-08-07
+"""
+__author__ = "dhilipsiva"
+__status__ = "development"
+
+"""
+Just a list of errors
+"""
+
+
+class AppknoxError(Exception):
+
+    def __init__(self, error_detail):
+        super(AppknoxError, self).__init__()
+        self.error_detail = error_detail
+
+    def __str__(self):
+        return "%s: %s" % (self.__class__.__name__, self.error_detail)
+
+
+class MissingCredentialsError(AppknoxError):
+
+    def __init__(self):
+        super(MissingCredentialsError, self).__init__(
+            "Please provide either `username/password`")
+
+
+class InvalidCredentialsError(AppknoxError):
+
+    def __init__(self):
+        super(InvalidCredentialsError, self).__init__(
+            "Your username / password pair is invalid."
+            " Please check username & password")
