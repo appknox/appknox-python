d9d81a9377556a9260766ed87c7bb70e3d5303ce
dhilipsiva <dhilipsiva@gmail.com>
2015-09-09 13:33:16 +0530

    Bump version for updated docs

--- setup.py
+++ setup.py
@@ -25,12 +25,12 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name='appknox',
-    version='0.0.3',
+    version='0.0.4',
     description="Python wrapper for Appknox's REST API",
     long_description=long_description,
     url='https://github.com/appknox/appknox-python',
