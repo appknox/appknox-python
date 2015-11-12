b661a9533fbaf7e785a2d58b77f61bdb944877f1
Subho Halder <subho.halder@gmail.com>
2015-11-12 07:30:14 +0530

    Fix errors and linting

--- appknox/cli.py
+++ appknox/cli.py
@@ -171,5 +171,5 @@ def report(config, file_id, format_type):
     response = config.client.report(file_id, format_type)
     if not format_type == 'pdf':
         return pprint(response)
-    with open(file_id+'-report.pdf', 'w') as f:
+    with open(file_id + '-report.pdf', 'w') as f:
         f.write(response)
