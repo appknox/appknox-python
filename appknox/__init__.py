3743d01267b9f1b2e7d6a3b3ac656b0fb68c2f1d
darshanime <deathbullet@gmail.com>
2017-05-04 13:26:48 +0530

    import constants in __init__.py

--- appknox/__init__.py
+++ appknox/__init__.py
@@ -14,7 +14,11 @@ Date created: 2015-08-07
 __author__ = "dhilipsiva"
 __status__ = "development"
 
-from appknox.client import AppknoxClient, DEFAULT_APPKNOX_URL
+from appknox.client import AppknoxClient
+from appknox.constants import DEFAULT_APPKNOX_URL, DEFAULT_PROJECT_LIMIT, \
+    DEFAULT_PROJECT_OFFSET, DEFAULT_FILE_LIMIT, DEFAULT_FILE_OFFSET, \
+    DEFAULT_REPORT_FORMAT, DEFAULT_REPORT_LANGUAGE, \
+    DEFAULT_VULNERABILITY_LANGUAGE, DEFAULT_LOG_LEVEL, DEFAULT_SECURE_CONNECTION
 from appknox.errors import AppknoxError, MissingCredentialsError, \
     InvalidCredentialsError, ResponseError
 import appknox.cli
