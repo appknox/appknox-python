3aaeac70963d89ff389feb5ed3e59e5dfed9deb1
Viren Nadkarni <viren@outlook.com>
2017-06-21 14:42:33 +0530

    Update metafiles

--- DESCRIPTION.rst
+++
@@ -1,8 +0,0 @@
-A Python wrapper for @appknox's REST API
-========================================
-
-This is a client library around Appknox's REST API.
-
-Please look at the `README.md <https://github.com/appknox/appknox-python/blob/master/README.md/>`_.
-
-DOCS: https://github.com/appknox/appknox-python/blob/master/README.md
--- setup.py
+++ setup.py
@@ -1,70 +1,40 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-#
-# vim: fenc=utf-8
-# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
-#
-#
+# (c) 2017, XYSec Labs
 
-"""
-Python wrapper for Appknox's REST API
-File name: setup.py
-Version: 0.1
-Author: dhilipsiva <dhilipsiva@gmail.com>
-Date created: 2015-08-07
-"""
-
-from setuptools import setup, find_packages
-# from codecs import open
 from os import path
+from pip.req import parse_requirements
+from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
-"""
-try:
-    f = path.join(here, 'README.md')
-    from pypandoc import convert
-    long_description = convert(f, 'rst')
-except IOError:
-    print("Cannot read Readme.md file")
-except ImportError:
-    print(
-        "pypandoc module not found, could not convert Markdown to RST")
-    long_description = open(f, 'r').read()
-"""
+with open('requirements.txt') as f:
+    install_requires = f.read().splitlines()
 
 setup(
     name='appknox',
-    version='1.4.0',
-    description="Python wrapper for Appknox's REST API",
-    long_description="Python wrapper for Appknox's REST API",
+    version='2.0.0',
+    description='Python wrapper for Appknox API',
+    long_description='Python wrapper for Appknox API',
     url='https://github.com/appknox/appknox-python',
-    author='dhilipsiva',
-    author_email='dhilipsiva@gmail.com',
+    author='Appknox',
+    author_email='engineering@appknox.com',
     license='MIT',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
     ],
 
-    keywords='appknox xysec rest api wrapper',
+    keywords='appknox xysec rest api wrapper cli mobile security',
     packages=find_packages(),
     py_modules=['appknox'],
     entry_points='''
         [console_scripts]
         appknox=appknox.cli:cli
     ''',
-    install_requires=[
-        'requests',
-        'click',
-    ],
+    install_requires=install_requires,
     extras_require={
         'dev': [''],
         'test': [''],
