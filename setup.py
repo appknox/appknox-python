cb5e6862c1c5ba914ea6c41fd266f1303225c3b1
dhilipsiva <dhilipsiva@gmail.com>
2017-07-03 15:15:03 +0530

    Udpate Version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='appknox',
-    version='2.0.1',
+    version='2.1.0',
     description='Python wrapper for Appknox API',
     long_description='Python wrapper for Appknox API',
     url='https://github.com/appknox/appknox-python',
