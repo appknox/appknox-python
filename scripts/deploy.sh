54b19c8afc5e0bb41545e90d61eee686ea70429e
Subho Halder <subho.halder@gmail.com>
2017-12-07 00:15:41 +0530

    use semantic version

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,13 @@
 
 set -xeuo pipefail
 
+export CURRENT_BRANCH
+CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git push origin "$CURRENT_BRANCH:$CURRENT_BRANCH"
+git push --tags
+
+bumpversion patch
+
 rm -rf dist/
 python setup.py sdist
 python setup.py bdist_wheel
