8c472d81e80f32c82972c07c94a25e6e9b0432c9
Subho Halder <subho.halder@gmail.com>
2017-12-22 17:30:46 +0530

    Bump version: 2.1.3 → 2.1.4

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='appknox',
-    version='2.1.3',
+    version='2.1.4',
     description='Python wrapper for Appknox API',
     long_description='Python wrapper for Appknox API',
     url='https://github.com/appknox/appknox-python',
