0fc6c0db4e9bfc4450173c7bd29c66f84fb9b5c0
Subho Halder <subho.halder@gmail.com>
2021-03-24 20:56:05 +0530

    Added proxy support in client

--- appknox/client.py
+++ appknox/client.py
@@ -37,7 +37,8 @@ class Appknox(object):
         self, username: str = None, password: str = None, user_id: int = None,
         organization_id: int = None, token: str = None,
         access_token: str = None, host: str = DEFAULT_API_HOST,
-        log_level: int = logging.INFO
+        log_level: int = logging.INFO, http_proxy: str = None,
+        https_proxy: str = none
     ):
         """
         Initialise Appknox client
@@ -61,6 +62,16 @@ class Appknox(object):
         self.organization_id = organization_id
         self.token = token
         self.access_token = access_token
+        self.proxies = {}
+
+        if http_proxy:
+            self.proxies['http'] = http_proxy
+
+        if https_proxy:
+            self.proxies['https'] = https_proxy
+
+        if not proxies:
+            self.proxies = None
 
         if self.host is None:
             self.host = DEFAULT_API_HOST
@@ -71,11 +82,13 @@ class Appknox(object):
             }
             self.json_api = ApiResource(
                 host=self.host,
-                headers={**JSON_API_HEADERS, **token_header}
+                headers={**JSON_API_HEADERS, **token_header},
+                proxies=self.proxies,
             )
             self.drf_api = ApiResource(
                 host=self.host,
-                headers={**DRF_API_HEADERS, **token_header}
+                headers={**DRF_API_HEADERS, **token_header},
+                proxies=self.proxies,
             )
             self.organization_id = self.get_organization_id()
 
@@ -83,12 +96,14 @@ class Appknox(object):
             self.json_api = ApiResource(
                 host=self.host,
                 headers={**JSON_API_HEADERS},
-                auth=(self.user_id, self.token)
+                auth=(self.user_id, self.token),
+                proxies=self.proxies,
             )
             self.drf_api = ApiResource(
                 host=self.host,
                 headers={**DRF_API_HEADERS},
-                auth=(self.user_id, self.token)
+                auth=(self.user_id, self.token),
+                proxies=self.proxies,
             )
             self.organization_id = self.get_organization_id()
 
@@ -110,7 +125,8 @@ class Appknox(object):
         if otp:
             data['otp'] = str(otp)
 
-        response = requests.post(urljoin(self.host, 'api/login'), data=data)
+        response = requests.post(
+            urljoin(self.host, 'api/login'), data=data, proxies=self.proxies)
 
         if response.status_code == 401:
             raise OneTimePasswordError(response.json()['message'])
@@ -176,7 +192,8 @@ class Appknox(object):
                 'name': 'appknox-python for {} @{}'.format(
                     self.username, str(int(time.time()))
                 )
-            }
+            },
+            proxies=self.proxies,
         )
         return mapper_json_api(PersonalToken, access_token.json())
 
@@ -186,7 +203,8 @@ class Appknox(object):
         """
         resp = requests.get(
             urljoin(self.host, 'api/personaltokens?key=' + self.access_token),
-            auth=(self.user_id, self.token)
+            auth=(self.user_id, self.token),
+            proxies=self.proxies,
         )
         resp_json = resp.json()
         personal_token = next((p for p in resp_json.get('data')), None)
@@ -196,7 +214,8 @@ class Appknox(object):
         token_id = personal_token['id']
         return requests.delete(
             urljoin(self.host, 'api/personaltokens/' + token_id),
-            auth=(self.user_id, self.token)
+            auth=(self.user_id, self.token),
+            proxies=self.proxies
         )
 
     def get_user(self, user_id: int) -> User:
@@ -421,7 +440,7 @@ class Appknox(object):
             'organizations/{}/upload_app'.format(self.organization_id)
         ]().get()
         url = response['url']
-        requests.put(url, data=file_data)
+        requests.put(url, data=file_data, proxies=self.proxies)
         response2 = self.drf_api[
             'organizations/{}/upload_app'.format(self.organization_id)
         ]().post(
@@ -518,11 +537,12 @@ class Appknox(object):
 class ApiResource(object):
     def __init__(
         self, host: str = DEFAULT_API_HOST, headers: object = None,
-        auth: Dict[str, str] = None
+        auth: Dict[str, str] = None, proxies: object = None
     ):
         self.host = host
         self.headers = {**headers}
         self.auth = auth
+        self.proxies = proxies
 
         self.endpoint = urljoin(host, API_BASE)
 
@@ -541,19 +561,20 @@ class ApiResource(object):
     def get(self, **kwargs):
         resp = requests.get(
             self.endpoint, headers=self.headers, auth=self.auth,
-            params=kwargs
+            proxies=self.proxies, params=kwargs
         )
         return resp.json()
 
     def post(self, data, content_type=None, **kwargs):
         resp = requests.post(
             self.endpoint, headers=self.headers, auth=self.auth,
-            params=kwargs, data=data
+            proxies=self.proxies, params=kwargs, data=data
         )
         return resp.json()
 
     def direct_get(self, url, **kwargs):
         resp = requests.get(
-            url, headers=self.headers, auth=self.auth, params=kwargs
+            url, headers=self.headers, auth=self.auth, params=kwargs,
+            proxies=self.proxies,
         )
         return resp.json()
